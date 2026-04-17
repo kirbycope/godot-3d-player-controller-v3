60459b3868d730e53c6bc4b4280b8c70690daa04
Timothy Cope <kirbycope@gmail.com>
2026-04-17 11:35:17 -0700

    wip

--- addons/3d_player_controller/player.gd
+++ addons/3d_player_controller/player.gd
@@ -1,10 +1,24 @@
 class_name Player
 extends CharacterBody3D
 
+@export var animation_tree: AnimationTree;
+@export var jump_velocity: float = 4.5
+@export var locomotionBlendPath: String ## AnimationTree Path: Root > LocomotionStateMachine > Locomotion
+@export var speed: float = 5.0
+@export var transition_speed: float = 0.1
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@onready var player_model: Node3D = $PlayerModel
 
+var input_dir: Vector2 ## The current [Input] vector
+
+
+func _process(delta: float) -> void:
+	# Sync player input and state machine's blend values
+	animation_tree.set(locomotionBlendPath, input_dir)
+
+	# DEBUGGING
+	$Debug/List/Input/X.text = "X: "+  str(input_dir.x)
+	$Debug/List/Input/Y.text = "Y: " + str(input_dir.y)
 
 
 func _ready() -> void:
@@ -21,17 +35,17 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
