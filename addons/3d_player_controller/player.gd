19eae873f4b2e9c3bc933fd66c2a7b5cc4d0aa6a
Timothy Cope <kirbycope@gmail.com>
2026-04-19 21:49:52 -0700

    wip

--- addons/3d_player_controller/player.gd
+++ addons/3d_player_controller/player.gd
@@ -1,6 +1,6 @@
 class_name Player
 extends CharacterBody3D
-
+# https://youtu.be/l4uWdObc4do?si=-nYMz-615jmEC9qs&t=402
 @export var animation_tree: AnimationTree;
 @export var jump_velocity: float = 4.5
 @export var locomotionBlendPath: String ## AnimationTree Path: Root > LocomotionStateMachine > Locomotion
@@ -9,16 +9,23 @@ extends CharacterBody3D
 
 @onready var player_model: Node3D = $Pivot/RootMotion/PlayerModel
 
-var input_dir: Vector2 ## The current [Input] vector
+var current_input_vector: Vector2 ## The current [Input] vector
+var current_velocity: Vector2 ## The current velocity of the player (no verticality)
 
 
 func _process(delta: float) -> void:
+	var new_delta = current_input_vector - current_velocity
+	if (new_delta.length() > transition_speed * delta):
+		new_delta = new_delta * transition_speed * delta
+	current_velocity += new_delta
+	animation_tree.set(locomotionBlendPath, current_velocity)
+
 	# Sync player input and state machine's blend values
-	animation_tree.set(locomotionBlendPath, input_dir)
+	#animation_tree.set(locomotionBlendPath, current_input_vector)
 
 	# DEBUGGING
-	$Debug/List/Input/X.text = "X: "+  str(input_dir.x)
-	$Debug/List/Input/Y.text = "Y: " + str(input_dir.y)
+	$Debug/List/Input/X.text = "X: "+  str(current_input_vector.x)
+	$Debug/List/Input/Y.text = "Y: " + str(current_input_vector.y)
 
 
 func _ready() -> void:
@@ -39,8 +46,8 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	current_input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(current_input_vector.x, 0, current_input_vector.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
