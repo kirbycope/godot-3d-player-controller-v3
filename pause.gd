74bd0ee6a643b2d1bb7b12c3cf2a245b24a86e86
Timothy Cope <kirbycope@gmail.com>
2026-05-25 22:32:40 -0700

    project

--- pause.gd
+++ pause.gd
@@ -8,7 +8,8 @@ func _ready() -> void:
 	$"../Player/Debug".hide()
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed:
+	if (event is InputEventMouseButton and event.pressed) \
+	or (event is InputEventScreenTouch and event.pressed):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		$"../Player/Controls".show()
 		$"../Player/Debug".show()
