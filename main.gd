80534d7a3cc9765d2530eed1f70c5252c784f957
Timothy Cope <kirbycope@gmail.com>
2026-05-25 19:20:44 -0700

    wip

--- main.gd
+++ main.gd
@@ -6,6 +6,11 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
+func _process(delta: float) -> void:
+	$ClimbingWall/HowToStartClimbing.visible = not $Player.is_climbing
+	$ClimbingWall/HowToClimb.visible = $Player.is_climbing
+
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player:
 		$Player.global_position = $"./CSGBox3D3/Marker3D".global_position#Vector3(0.0, 10.0, 4.0)
