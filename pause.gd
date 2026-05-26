a0304cb830f02fc0666fa2a6e07395c6e288c20c
Timothy Cope <kirbycope@gmail.com>
2026-05-25 21:26:26 -0700

    web build

---
+++ pause.gd
@@ -0,0 +1,16 @@
+extends ColorRect
+
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	get_tree().call_deferred("set", "paused", true)
+	$"../Player/Controls".hide()
+	$"../Player/Debug".hide()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		$"../Player/Controls".show()
+		$"../Player/Debug".show()
+		get_tree().paused = false
+		hide()
