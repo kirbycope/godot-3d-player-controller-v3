84d1ec2c6eb44121f37da3cc19111fbf6250e170
Timothy Cope <kirbycope@gmail.com>
2026-05-29 10:26:02 -0700

    wood sign

--- wooden_sign.gd
+++ wooden_sign.gd
@@ -1,13 +1,26 @@
 extends Node3D
 
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 @onready var look_at_target: Node3D = $LookAtTarget
 
-var player: Player
+var is_read: bool = false ## Has the player read this sign?
+var player: Player ## Cached reference to the Player
 
 
 func _input(event: InputEvent) -> void:
-	if player and event.is_action_pressed("action"):
-		print("Player is interacting with: ", self.name)
+	# Show initial dialog
+	if player \
+	and event.is_action_pressed("action") \
+	and not canvas_layer.visible \
+	and not is_read:
+		canvas_layer.show()
+	# Advance dialog
+	elif player \
+	and event.is_action_pressed("action") \
+	and canvas_layer.visible:
+		canvas_layer.hide()
+		is_read = true
+		player.look_at_modifier.target_node = NodePath("")
 
 
 func _on_player_detection_body_entered(body: Node3D) -> void:
@@ -21,5 +34,7 @@ func _on_player_detection_body_entered(body: Node3D) -> void:
 
 func _on_player_detection_body_exited(body: Node3D) -> void:
 	if body is Player:
+		canvas_layer.hide()
+		is_read = false
 		player.look_at_modifier.target_node = NodePath("")
 		player = null
