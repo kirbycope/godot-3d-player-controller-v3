3f89a848f69c34148ac8377870fc9efa20883ab0
Timothy Cope <kirbycope@gmail.com>
2026-07-21 15:07:09 -0700

    moon

---
+++ scenes/moon.gd
@@ -0,0 +1,25 @@
+extends StaticBody3D
+
+var players_in_range: Array[Player] = []
+
+
+func _physics_process(_delta: float) -> void:
+	for i in range(players_in_range.size() - 1, -1, -1):
+		var player := players_in_range[i]
+		if is_instance_valid(player):
+			var surface_up := (player.global_position - global_position).normalized()
+			if surface_up.length_squared() > 0.001:
+				player.up_direction = surface_up
+		else:
+			players_in_range.remove_at(i)
+
+
+func _on_player_detection_body_entered(body: Node3D) -> void:
+	if body is Player and not players_in_range.has(body):
+		players_in_range.append(body)
+
+
+func _on_player_detection_body_exited(body: Node3D) -> void:
+	if body is Player:
+		players_in_range.erase(body)
+		body.up_direction = Vector3.UP
